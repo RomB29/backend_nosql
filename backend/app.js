28523fc189a4ccde7875ab75f1a1fafddb886621
Romain <81963300+RomB29@users.noreply.github.com>
2022-09-03 17:22:46 +0200

    CRUD completed

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // //     next();
 // //   }
 // });
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -59,13 +60,38 @@ app.post("/api/stuff", (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
         ...req.body
+        /*
+        equivalent too:
+        title: req.body.title,
+        description: req.body.description,
+        ....
+        */
     });
     thing.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
         .catch(error => res.status(400).json({ error }));
 });
 
-app.use("/api/stuff", (req, res, next) => {
+app.put('/api/stuff/:id', (req, res, next) => { // Modify object
+
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req,res, next) => { // delete object
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé'}))
+        .catch(error => res.status(400).json({ error}));
+});
+
+app.get("/api/stuff/:id", (req, res, next) => { // get specific ID information
+    Thing.findOne({ _id: req.params.id}) // Filter on ID
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.get("/api/stuff", (req, res, next) => {
 //   const stuff = [
 //     {
 //       _id: "1",
@@ -88,7 +114,6 @@ app.use("/api/stuff", (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
-//   res.status(200).json(stuff);
 });
 
 module.exports = app;
